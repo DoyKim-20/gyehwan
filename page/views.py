3e786ae0b2ed3ff5193621756b55679a0a8889f8
DoyKim-20 <diana7533@naver.com>
2020-07-20 00:26:38 +0900

    why...?

--- page/views.py
+++ page/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from .models import Content
-# Create your views here.
+
 def home(request):
-    contents = Content.objects.all()
-    return render(request,'home.html',{'contents':contents})
+    return render(request,'home.html')
+# Create your views here.
